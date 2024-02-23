e90e677b5c69a926ecf012c6c1268a628349ea40
Amrutha-V-S <Amrutha.V.S@ibm.com>
2024-02-23 14:57:06 +0530

    auth change complete

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -25,44 +25,52 @@ const login = async (credentials) => {
       throw new Error("Failed to login!");
     }
   };
-export const {handlers:{GET,POST},auth,signIn,signOut}=NextAuth(
-    {
-        providers: [
-            GitHub({clientId:process.env.GITHUB_ID,clientSecret:process.env.GITHUB_SECRET}),           
-              CredentialsProvider({
-                async authorize(credentials) {
-                  try {
-                    const user = await login(credentials);
-                    return user;
-                  } catch (err) {
-                    return null;
-                  }
-                },
-              }),
-        ]
-    ,
-    callbacks:{
-        async signIn({user,account,profile}){
-            if(account.provider==='github'){
-                connectToDb()
-                try{
-                    const user=await User.findOne({email:profile.email});
-                    if(!user){
-                        const newUser=new User({
-                            username:profile.login,
-                            email:profile.email,
-                            image:profile.avatar_url
-                        })
-                        await newUser.save()
-                    }else{
-
-                    }
-                }catch(err){
-                    console.log(err)
-                    return false
-                }
-                return true
+  export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+  } = NextAuth({
+    //...authConfig,
+    providers: [
+      GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+      CredentialsProvider({
+        async authorize(credentials) {
+          try {
+            const user = await login(credentials);
+            return user;
+          } catch (err) {
+            return null;
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      async signIn({ user, account, profile }) {
+        if (account.provider === "github") {
+          connectToDb();
+          try {
+            const user = await User.findOne({ email: profile.email });
+  
+            if (!user) {
+              const newUser = new User({
+                username: profile.login,
+                email: profile.email,
+                image: profile.avatar_url,
+              });
+  
+              await newUser.save();
             }
+          } catch (err) {
+            console.log(err);
+            return false;
+          }
         }
-    }
-})
+        return true;
+      },
+    //  ...authConfig.callbacks,
+    },
+  });
